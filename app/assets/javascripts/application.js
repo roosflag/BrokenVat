4d2c08de8337d06c6823abcdccaa2be1e4d8f717
Sean Peel <phatmoneky@gmail.com>
2016-12-21 20:48:42 -0600

    updated logo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,56 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
-(function($) {
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 200) {
-                $('#navbar').fadeIn(500);
-            } else {
-                $('#navbar').fadeOut(500);
-            }
-        });
+// (function($) {
+//     $(document).ready(function(){
+//         $(window).scroll(function(){
+//             if ($(this).scrollTop() > 200) {
+//                 $('#mainnav').fadeIn(500);
+//             } else {
+//                 $('#mainnav').fadeOut(500);
+//             }
+//         });
+//     });
+// })(jQuery);
+// $(document).on('turbolinks:load', function() {
+//
+//     var mainnav = $('.main_nav');
+//     var origOffsetY = mainnav.offset().top;
+//
+//     function scroll() {
+//         if ($(window).scrollTop() >= origOffsetY) {
+//             $('.main_nav').addClass('sticky');
+//             $('.moon').addClass('main_nav-padding');
+//         } else if (true)  {
+//             $('.main_nav').removeClass('sticky');
+//             $('.moon').removeClass('main_nav-padding');
+//         }
+//
+//
+//     }
+//
+//     document.onscroll = scroll;
+//
+// });
+$(document).on('turbolinks:load', function() {
+    "use strict";
+
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+                top: 0
+            });
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "48px"
+            }, 0);
+        }
     });
-})(jQuery);
+
+})(jQuery, undefined);
